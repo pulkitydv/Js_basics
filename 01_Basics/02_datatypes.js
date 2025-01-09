4d6e09af1ffbc1101af47c9c3fbd3867a144576a
Pulkit Yadav <133958902+pulkitydv@users.noreply.github.com>
2025-01-09 07:59:00 +0000

    Basic datatypes in js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //can't execute because we use nodejs not the browser , here its syntax is diff
+
+let name = "Pulkit"
+let age = 18
+let isLoggedIn = false
+let state; //undefined
+
+// number => 2 to power 53 (approx) range
+// bigint    // use for larger number
+//string => ""
+//boolean => true/false 
+//null => standalone value
+//undefined 
+//symbol => to find uniqueness between components in mostly react 
+// object 
+
+console.log(typeof "Pulkit"); 
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
